22587790c7a17614a53fbb053d5fe8327e28244e
impu1se <impu1se@bk.ru>
2016-10-10 12:05:54 +0300

    half

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,32 @@
+import sys
 import json
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print('Файл не найден')
+        sys.exit(1)
+
 
 
 def pretty_print_json(data):
-    pass
+    result = None
+    if isinstance(data, list):
+        result = ['[\n\t']
+        for i in data:
+            if isinstance(i, dict):
+                result += '{\n\t' + i + ':'
+                for key, value in i:
+                    while isinstance(value, dict):
+
+
+
+
 
 
 if __name__ == '__main__':
-    pass
+    open_file = load_data('magazine.json')
+    pretty_print_json(open_file)
