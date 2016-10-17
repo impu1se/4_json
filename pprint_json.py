f878ac32b52a1517978d2f6ea99bec9e4e1fdfd8
impu1se <impu1se@bk.ru>
2016-10-17 17:43:45 +0300

    finally3

--- pprint_json.py
+++ pprint_json.py
@@ -1,21 +1,16 @@
-import sys
 import json
 import argparse
 
 
 parser = argparse.ArgumentParser(description='Pretty print for JSON')
 parser.add_argument('-file', dest='filepath',
-                    help='Input filepath and filename with expansion json' )
+                    help='Input filepath and filename with expansion json')
 args = parser.parse_args()
 
 
 def load_data(filepath):
-    try:
-        with open(filepath, encoding='utf-8') as f:
+    with open(filepath, encoding='utf-8') as f:
             return json.load(f)
-    except (FileNotFoundError, ValueError):
-        print('Файл не найден')
-        sys.exit(1)
 
 
 def pretty_print_json(data):
@@ -23,5 +18,11 @@ def pretty_print_json(data):
 
 
 if __name__ == '__main__':
-    open_file = load_data(args.filepath)
-    print(pretty_print_json(open_file))
+    parser = argparse.ArgumentParser(description='Pretty print for JSON')
+    parser.add_argument('-file', dest='filepath',
+                        help='Input filepath and filename with expansion json')
+    args = parser.parse_args()
+    try:
+        print(pretty_print_json(load_data(args.filepath)))
+    except FileNotFoundError:
+        print('File not found')
